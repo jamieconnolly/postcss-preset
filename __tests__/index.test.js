8bb40756437a4d342a82af95959beb27758268cc
Jamie Connolly <jamie@jamieconnolly.com>
2017-12-21 17:31:21 +0000

    :white_check_mark: Add in some preset validation tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,21 @@
 'use strict';
 
 const isPlainObj = require('is-plain-obj');
+const postcss = require('postcss');
+const postcssrc = require('postcss-load-config');
 
 it('should export an object', () => {
   const config = require('../index');
-
   expect(isPlainObj(config)).toBe(true);
+});
+
+it('should not contain invalid queries', () => {
+  return postcssrc({}, '__tests__/fixtures')
+    .then((config) => {
+      return postcss(config.plugins)
+        .process('a { font-weight: 500; }\n', config.options)
+        .then((result) => {
+          expect(result).toBeTruthy();
+        });
+    });
 });
